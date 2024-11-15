60db8f827168629e1597cc329d13ab5875698a4d
alptwy <alpheustwy@gmail.com>
2024-11-15 23:57:30 +0800

    Update llm.py

--- helper_functions/llm.py
+++ helper_functions/llm.py
@@ -5,10 +5,9 @@ from dotenv import load_dotenv
 from openai import OpenAI
 import tiktoken
 
-if load_dotenv('.env')
-   OPENAI_KEY = os.getenv('OPENAI_API_KEY')
-else;
-   OPENAI_KEY = st.secrets['OPENAI_API_KEY']
+
+# Pass the API Key to the OpenAI Client
+client = OpenAI(api_key=st.secrets('OPENAI_API_KEY'))
                             
 # Pass the API Key to the OpenAI Client
 client = OpenAI(api_key=OPENAI_KEY')
